28dd7e254a91b43de3d6da461f7f7da08dd58cc9
edwardboileau <edwardboileau@gmail.com>
2023-11-28 14:16:16 +0000

    Removed errors + warnings from distributor.go and reduced amount of function calls. Now runs a bit faster.

--- gol/distributor.go
+++ gol/distributor.go
@@ -32,8 +32,8 @@ func filenameOutput(p Params) string {
 	return s
 }
 
-//recieves an array of bytes from ioInput
-func recieveworld(ioInput <-chan uint8, p Params, events chan<- Event) [][]byte {
+//receives an array of bytes from ioInput
+func receiveWorld(ioInput <-chan uint8, p Params, events chan<- Event) [][]byte {
 	world := make([][]byte, p.ImageHeight)
 	for i := range world {
 		world[i] = make([]byte, p.ImageWidth)
@@ -45,7 +45,7 @@ func recieveworld(ioInput <-chan uint8, p Params, events chan<- Event) [][]byte 
 			//	fmt.Println(x, y)
 			//}
 			if val != 0 {
-				events <- CellFlipped{0, util.Cell{x, y}}
+				events <- CellFlipped{0, util.Cell{X: x, Y: y}}
 			}
 			world[y][x] = val
 		}
@@ -54,63 +54,74 @@ func recieveworld(ioInput <-chan uint8, p Params, events chan<- Event) [][]byte 
 }
 
 //sends world to ioOutput
-func sendsworld(ioOutput chan<- uint8, world [][]byte) {
+func sendWorld(ioOutput chan<- uint8, world [][]byte) {
 	for _, row := range world {
 		for _, v := range row {
 			ioOutput <- v
-			if v != 0 {
-				//fmt.Println("send coords:", strconv.Itoa(x), strconv.Itoa(y))
-			}
 		}
 	}
 }
 
 //GOL logic
-//count3x3 counts how many live cells there are in a 3 x 3 area centered around some x y
-func count3x3(grid [][]byte, x, y int, params Params) int {
-	//fmt.Println(x, y)
-	count := 0
-	for xi := -1; xi < 2; xi++ {
-		xi2 := x + xi
-		//fmt.Println("xi:", xi)
-		xi2 = edgereset(xi2, params.ImageWidth)
-		//fmt.Println("xi2:", xi2)
-		for yi := -1; yi < 2; yi++ {
-			//fmt.Println("yi:", yi)
-			yi2 := yi + y
-			yi2 = edgereset(yi2, params.ImageHeight)
-			//fmt.Println("yi:", yi2)
-			if grid[yi2][xi2] == 255 {
-				count += 1
-			}
-		}
+
+func numAdjacentLiving(world [][]byte, x int, y int, imageWidth int, imageHeight int) int {
+	var numAdjacentLiving = 0
+	if (x > imageWidth) || (y > imageHeight) || (0 > x) || (0 > y) {
+		panic("Co-ordinates out of range!")
 	}
-	return count
+	var cellToLeftX, cellToRightX, cellAboveY, cellBelowY int
+
+	//get cells next to target cell, wrapping around if necessary
+	if x == 0 {
+		cellToLeftX = imageWidth - 1
+	} else {
+		cellToLeftX = x - 1
+	}
+	if x == imageWidth-1 {
+		cellToRightX = 0
+	} else {
+		cellToRightX = x + 1
+	}
+	if y == 0 {
+		cellBelowY = imageHeight - 1
+	} else {
+		cellBelowY = y - 1
+	}
+	if y == imageHeight-1 {
+		cellAboveY = 0
+	} else {
+		cellAboveY = y + 1
+	}
+
+	//increment numAdjacentLiving for each adjacent living cell
+	if world[cellToLeftX][y] == 255 {
+		numAdjacentLiving++
+	}
+	if world[cellToLeftX][cellAboveY] == 255 {
+		numAdjacentLiving++
+	}
+	if world[x][cellAboveY] == 255 {
+		numAdjacentLiving++
+	}
+	if world[cellToRightX][cellAboveY] == 255 {
+		numAdjacentLiving++
+	}
+	if world[cellToRightX][y] == 255 {
+		numAdjacentLiving++
+	}
+	if world[cellToRightX][cellBelowY] == 255 {
+		numAdjacentLiving++
+	}
+	if world[x][cellBelowY] == 255 {
+		numAdjacentLiving++
+	}
+	if world[cellToLeftX][cellBelowY] == 255 {
+		numAdjacentLiving++
+	}
+
+	return numAdjacentLiving
 }
 
-//if out of array loops the value back around again
-func edgereset(i int, max int) int {
-	if i < 0 {
-		return (max - 1)
-	}
-	if i >= max {
-		return 0
-	}
-	return i
-}
-
-//cell value should return the value of a cell given its count
-func cellValue(count int, cellvalue byte) byte {
-	switch count {
-	case 3:
-		return 255
-	case 4:
-		if cellvalue != 0 {
-			return 255
-		}
-	}
-	return 0
-}
 func partWorld(turn, startY, endY, startX, endX int, world, newWorld [][]byte, params Params, group *sync.WaitGroup, events chan<- Event) {
 	stageConverter(turn, startY, endY, startX, endX, world, newWorld, params, events)
 	group.Done()
@@ -119,28 +130,37 @@ func stageConverter(turn, startY, endY, startX, endX int, world, newWorld [][]by
 
 	for y := startY; y < endY; y++ {
 		for x := startX; x < endX; x++ {
-			newWorld[y][x] = cellValue(count3x3(world, x, y, params), world[y][x])
+			var numAdjacentLiving = numAdjacentLiving(world, x, y, params.ImageWidth, params.ImageHeight)
+			switch {
+			case ((numAdjacentLiving < 2) || (numAdjacentLiving > 3)) && (world[x][y] == 255):
+
+				newWorld[x][y] = 0
+			case (numAdjacentLiving == 3) && (world[x][y] == 0):
+				newWorld[x][y] = 255
+			default:
+				newWorld[x][y] = world[x][y]
+			}
 			if newWorld[y][x] != world[y][x] {
-				events <- CellFlipped{turn, util.Cell{x, y}}
+				events <- CellFlipped{turn, util.Cell{X: x, Y: y}}
 			}
 
 		}
 	}
 }
-func newworld(turn int, world, newWorld [][]byte, p Params, mutex *sync.Mutex, events chan<- Event) {
+func newWorld(turn int, world, newWorld [][]byte, p Params, mutex *sync.Mutex, events chan<- Event) {
 	mutex.Lock()
 	if p.Threads == 1 {
 		stageConverter(turn, 0, p.ImageHeight, 0, p.ImageWidth, world, newWorld, p, events)
 	} else {
 		var wg sync.WaitGroup
-		heightsplit := p.ImageHeight / p.Threads
+		heightSplit := p.ImageHeight / p.Threads
 		if p.ImageHeight%p.Threads != 0 {
-			heightsplit += 1
+			heightSplit += 1
 		}
 		for i := 0; i < p.Threads; i++ {
 			wg.Add(1)
-			startY := heightsplit * i
-			endY := heightsplit * (i + 1)
+			startY := heightSplit * i
+			endY := heightSplit * (i + 1)
 			if endY > p.ImageHeight {
 				endY = p.ImageHeight
 			}
@@ -151,8 +171,9 @@ func newworld(turn int, world, newWorld [][]byte, p Params, mutex *sync.Mutex, e
 	mutex.Unlock()
 }
 
+/*
 //useful for debugging reasons
-func printworld(world [][]byte) {
+func printWorld(world [][]byte) {
 	for _, v := range world {
 		var b []byte
 		for _, v2 := range v {
@@ -166,19 +187,20 @@ func printworld(world [][]byte) {
 	}
 	println("")
 }
+*/
 func aliveCells(world [][]byte) []util.Cell {
 
 	var aliveCells []util.Cell
 	for yi, row := range world {
 		for xi, value := range row {
 			if value == 255 {
-				aliveCells = append(aliveCells, util.Cell{xi, yi})
+				aliveCells = append(aliveCells, util.Cell{X: xi, Y: yi})
 			}
 		}
 	}
 	return aliveCells
 }
-func every2seconds(oddWorld, evenWorld [][]byte, turn *int, oddMutex, evenMutex, turnLock *sync.Mutex, events chan<- Event) {
+func every2Seconds(oddWorld, evenWorld [][]byte, turn *int, oddMutex, evenMutex, turnLock *sync.Mutex, events chan<- Event) {
 	for {
 		time.Sleep(2 * time.Second)
 		turnLock.Lock()
@@ -216,9 +238,9 @@ func waitKeypress(turn *int, worldOdd, worldEven [][]byte, turnLock *sync.Mutex,
 				c.ioCommand <- ioOutput
 				c.ioFilename <- strconv.Itoa(p.ImageWidth) + "x" + strconv.Itoa(p.ImageWidth) + "x" + strconv.Itoa(*turn)
 				if *turn%2 == 0 {
-					sendsworld(c.ioOutput, worldEven)
+					sendWorld(c.ioOutput, worldEven)
 				} else {
-					sendsworld(c.ioOutput, worldOdd)
+					sendWorld(c.ioOutput, worldOdd)
 				}
 				c.ioCommand <- ioCheckIdle
 				<-c.ioIdle
@@ -226,9 +248,9 @@ func waitKeypress(turn *int, worldOdd, worldEven [][]byte, turnLock *sync.Mutex,
 				c.ioCommand <- ioOutput
 				c.ioFilename <- strconv.Itoa(p.ImageWidth) + "x" + strconv.Itoa(p.ImageWidth) + "x" + strconv.Itoa(*turn)
 				if *turn%2 == 0 {
-					sendsworld(c.ioOutput, worldEven)
+					sendWorld(c.ioOutput, worldEven)
 				} else {
-					sendsworld(c.ioOutput, worldOdd)
+					sendWorld(c.ioOutput, worldOdd)
 				}
 				c.ioCommand <- ioCheckIdle
 				<-c.ioIdle
@@ -249,7 +271,7 @@ func distributor(p Params, c distributorChannels) {
 
 	c.ioCommand <- ioInput
 	c.ioFilename <- filenameInput(p)
-	worldEven := recieveworld(c.ioInput, p, c.events)
+	worldEven := receiveWorld(c.ioInput, p, c.events)
 
 	worldOdd := make([][]byte, p.ImageHeight)
 	for i := range worldOdd {
@@ -261,14 +283,14 @@ func distributor(p Params, c distributorChannels) {
 	//controls access to turn
 	var turnLock = &sync.Mutex{}
 	turn := 0
-	go every2seconds(worldOdd, worldEven, &turn, oddMutex, EvenMutex, turnLock, c.events)
+	go every2Seconds(worldOdd, worldEven, &turn, oddMutex, EvenMutex, turnLock, c.events)
 	go waitKeypress(&turn, worldOdd, worldEven, turnLock, c, p)
 	for turn < p.Turns {
 		turnLock.Lock()
 		if turn%2 == 0 {
-			newworld(turn, worldEven, worldOdd, p, EvenMutex, c.events)
+			newWorld(turn, worldEven, worldOdd, p, EvenMutex, c.events)
 		} else {
-			newworld(turn, worldOdd, worldEven, p, oddMutex, c.events)
+			newWorld(turn, worldOdd, worldEven, p, oddMutex, c.events)
 		}
 		c.events <- TurnComplete{turn}
 		turn++
@@ -278,10 +300,10 @@ func distributor(p Params, c distributorChannels) {
 	c.ioCommand <- ioOutput
 	c.ioFilename <- filenameOutput(p)
 	if turn%2 != 0 {
-		sendsworld(c.ioOutput, worldOdd)
+		sendWorld(c.ioOutput, worldOdd)
 		c.events <- FinalTurnComplete{turn, aliveCells(worldOdd)}
 	} else {
-		sendsworld(c.ioOutput, worldEven)
+		sendWorld(c.ioOutput, worldEven)
 		c.events <- FinalTurnComplete{turn, aliveCells(worldEven)}
 	}
 
